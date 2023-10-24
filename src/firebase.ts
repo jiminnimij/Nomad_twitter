f7e2b82c907260bd147944e3852d70b486253e82
jimin <010709min@ewhain.net>
2023-10-24 20:10:16 +0900

    connect firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoQc_EQ-oEWJ98-CDaXLjdVpM7X6ZjiwY",
+  authDomain: "nwitter-reloaded-8bd76.firebaseapp.com",
+  projectId: "nwitter-reloaded-8bd76",
+  storageBucket: "nwitter-reloaded-8bd76.appspot.com",
+  messagingSenderId: "636138544212",
+  appId: "1:636138544212:web:46f7f264396f7455fa61c1"
+};
+
+const app = initializeApp(firebaseConfig);
